b553102e33f9c5a9f2bbf3615ee22788a42f5865
Edouard-Legoupil <edouard.legoupil@gmail.com>
2013-12-26 16:29:18 +0300

    add

---
+++ js/data-jo.js
@@ -0,0 +1,20 @@
+ /* All admin name must be in CAP */
+
+var rcode2reg = {
+    '1':'NORTH',
+    '2':'CENTRAL',
+    '3':'SOUTH'}
+
+var gcode2gov = {
+    '1704':'KARAK',
+    '1710':'MADABA',
+    '1705':'AMMAN',
+    '1703':'AL AQABAH',
+    '1706':'TAFILAH',
+    '1712':'ZARQA',
+    '1709':'MAAN',
+    '1701':'AJLOUN',
+    '1702':'ABALQA',
+    '1707':'IRBID',
+    '1711':'MAFRAQ',
+    '1708':'JARASH'}
